21fb91cc1a83a9758845285b922361ee50de7bf8
Xushen Ma <maxushen@gmail.com>
2020-06-05 12:56:42 +1000

    Basic framework to start working on Mapper

---
+++ src/components/mapper.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Mapper extends Component {
+    render() {
+        return (
+            <div>Mapper!!</div>
+        );
+    }
+}
+
+export default Mapper;
